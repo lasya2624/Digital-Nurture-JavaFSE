1f0ae2cad84457bfcca7f51f93a71069282d56b9
lasya2624 <chandralasya24@gmail.com>
2026-06-30 10:13:00 +0530

    week3 hands on 1

---
+++ orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,27 @@
+package com.cognizant.orm_learn;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(OrmLearnApplication.class,args);
+
+        CountryService service = context.getBean(CountryService.class);
+
+        List<Country> countries = service.getAllCountries();
+
+        countries.forEach(System.out::println);
+
+    }
+
+}
