caebbc0eda5e6b24139dfe7c6c327a8a79111bcd
lasya2624 <chandralasya24@gmail.com>
2026-07-08 15:29:36 +0530

    spring rest 1

--- spring-learn/src/main/java/com/cognizant/spring_learn/model/Country.java
+++ spring-learn/src/main/java/com/cognizant/spring_learn/model/Country.java
@@ -1,31 +1,42 @@
 package com.cognizant.spring_learn.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Country {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Country.class);
 
     private String code;
     private String name;
 
-    public Country() {
+    public Country(String name, String code) {
+        this.name = name;
+        this.code = code;
     }
 
-    public Country(String code, String name) {
-        this.code = code;
-        this.name = name;
+    public Country() {
+        LOGGER.debug("Inside Country Constructor.");
     }
 
     public String getCode() {
+        LOGGER.debug("Inside getCode()");
         return code;
     }
 
     public void setCode(String code) {
+        LOGGER.debug("Inside setCode()");
         this.code = code;
     }
 
     public String getName() {
+        LOGGER.debug("Inside getName()");
         return name;
     }
 
     public void setName(String name) {
+        LOGGER.debug("Inside setName()");
         this.name = name;
     }
 
