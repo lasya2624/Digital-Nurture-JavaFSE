917ee2aba61f354d01032d91e985a2658e6d25da
lasya2624 <chandralasya24@gmail.com>
2026-07-08 10:16:32 +0530

    commit

---
+++ springlearn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,34 @@
+package com.cognizant.springlearn.controller;
+
+//import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.model.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.info("END");
+
+        context.close();
+
+        return country;
+    }
+}
